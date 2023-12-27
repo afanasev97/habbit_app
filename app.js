b154957569fe1b46cba0963d0db9aba1bb42e10a
Ilia Afanasev <afanasev.ilya.a@yandex.ru>
2023-12-28 00:53:29 +0300

    new: Array task complete

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 var log = console.log;
+const tasks = ['Задача 1'];
+function addTask(task, taskArr) {
+	taskArr.push(task);
+};
+// classic:
+// function removeTask(task, taskArr) {
+// 	while (taskArr.includes(task)) {
+// 		taskArr.splice(taskArr.indexOf(task), 1);
+// 	};
+// };
+// recursive:
+function removeTask(task, taskArr) {
+	if (taskArr.includes(task)) {
+		taskArr.splice(taskArr.indexOf(task), 1);
+		return removeTask(task, taskArr)
+	};
+};
 
+function unshiftTask(task, taskArr) {
+	removeTask(task, taskArr);
+	taskArr.unshift(task);
+};
+
+module.exports = {
+    tasks,
+    addTask,
+    removeTask,
+    unshiftTask,
+    log
+};
